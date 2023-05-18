e980d6450339b237e0b9c19ecfd17c9e074b9831
adityapermono8 <adityapermono8@gmail.com>
2023-05-18 15:06:08 +0700

    Menambahkan logika menu dan pemrosesan pilihan berdasarkan case

--- testsinglelinkedlist/testsinglelinkedlist.cpp
+++ testsinglelinkedlist/testsinglelinkedlist.cpp
@@ -141,4 +141,46 @@ int main() {
 			cout << "3. Tampilkan Data" << endl;
 			cout << "4. Cari Data" << endl;
 			cout << "5. Keluar" << endl;
-			
+			cout << "Pilihan: " << endl;
+			cin >> pilihan;
+			switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukkan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+			}
+		}
